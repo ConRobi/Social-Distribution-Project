fc0a952e408b99933a19015366dcfda4bed039b9
Stuart Shelley <sshelley@ualberta.ca>
2025-02-13 16:34:49 -0700

    add create profile frontend, add profile backend

--- MaroonSocialDistribution/SocialDistribution/urls.py
+++ MaroonSocialDistribution/SocialDistribution/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # Create profile interface page
+    path("create-profile", views.create_profile, name="create-profile"),
+    # API endpoint for adding profile
+    path("add-profile", views.add_profile, name = "add-profile")
 ]
--- MaroonSocialDistribution/SocialDistribution/views.py
+++ MaroonSocialDistribution/SocialDistribution/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponse
+from .models import Author
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 def index(request):
     return render(request, "index.html")
 
 def create_profile(request):
-    pass
+    return render(request, "create_profile.html")
+
+@api_view(['POST'])
+def add_profile(request):
+    
+    display_name = request.POST.get("display_name")
+    github = request.POST.get("github")
+    profile_image = request.POST.get("profile_image")
+
+
+    Author.objects.create(display_name=display_name, profile_image=profile_image, github=github)
+
+    return redirect(reverse("polls:results", kwargs={"pk": profile_image.id}))
 
 def view_profile(request):
     pass
