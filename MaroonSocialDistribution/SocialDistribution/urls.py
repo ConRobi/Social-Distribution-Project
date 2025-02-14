a13d699933e9444c913e16fb04c6ae29354d5474
Stuart Shelley <sshelley@ualberta.ca>
2025-02-13 18:48:18 -0700

    add django rest framework, update create profile

--- MaroonSocialDistribution/SocialDistribution/urls.py
+++ MaroonSocialDistribution/SocialDistribution/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'SocialDistribution'
+
 urlpatterns = [
     path("", views.index, name="index"),
     # Create profile interface page
--- MaroonSocialDistribution/SocialDistribution/views.py
+++ MaroonSocialDistribution/SocialDistribution/views.py
@@ -20,7 +20,6 @@ def add_profile(request):
     github = request.POST.get("github")
     profile_image = request.POST.get("profile_image")
 
-
     Author.objects.create(display_name=display_name, profile_image=profile_image, github=github)
 
     return redirect(reverse("polls:results", kwargs={"pk": profile_image.id}))
