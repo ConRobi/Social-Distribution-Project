f3cdeb333c4549a2eeb1819a10cc958070a9af12
Stuart Shelley <sshelley@ualberta.ca>
2025-03-03 11:42:35 -0700

    remove unnecessary url prefix 'socialDistribution'

--- MaroonSocialDistribution/MaroonSocialDistribution/urls.py
+++ MaroonSocialDistribution/MaroonSocialDistribution/urls.py
@@ -23,7 +23,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('SocialDistribution', include("SocialDistribution.urls")),
+    path('', include("SocialDistribution.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-docs"),
 ]
